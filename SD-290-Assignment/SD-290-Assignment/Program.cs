4eacb1cc549df44231b8bde73e78be589e3d1bb8
asad <asadtahir9999@gmail.com>
2022-06-13 18:15:04 -0500

    question one completed

--- SD-290-Assignment/SD-290-Assignment/Program.cs
+++ SD-290-Assignment/SD-290-Assignment/Program.cs
@@ -1,50 +1,67 @@
-﻿
+﻿using System;
+using System.Text;
 
-using System;  //Libarary
-using System.Text;  //Namespace
-namespace VendorMachine
 {
-    class Machine
+
+ class program
+{
+    static string compress(string str)
     {
-
-        private int DAmount;
-        const int cost = 95;
-
-
-        public Machine()
+        if (str == null || str == "")//if string is empty return empty
         {
-            DAmount = 0;
+            return str;
         }
-        public void DCoin(int coinAmount)
+        StringBuilder sb = new StringBuilder();
+        char prevChar = str[0];
+        int count = 1;
+        for (int i = 1; i < str.Length; i++)
         {
-            DAmount += coinAmount; //Increment in Damount
-        }
-        public void GetDrink()
-        {
-            if (DAmount >= cost)   //cost is greater then the if work
+            if (str[i] == prevChar)
             {
-                Console.WriteLine("Your Change is {0} cents", DAmount - cost);  //Printing
+                count++;
             }
             else
-                Console.WriteLine("insert more coins");  //if cost is less 
-
+            {
+                sb.Append(prevChar); //insert char
+                sb.Append(count);//insert count
+                prevChar = str[i];//set previous to current char
+                count = 1;
+            }
         }
-        public void GetRefund()
+
+        sb.Append(prevChar);
+        sb.Append(count);
+        string result = sb.ToString();//create a string from builder
+
+        if (result.Length >= str.Length)
         {
-            Console.WriteLine("You were refunded {0}", DAmount); //Printing
-            DCoin(0);
+            return str;
         }
-
-
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("************ Hello from vendor Machine *************** ");//Starting
-            var Machine = new Machine();
-            Machine.DCoin(70);  //Pasing 70 to DCOIN
-            Machine.DCoin(3);   //passing 3 to DCOIN
-            Machine.GetDrink(); //calling Getdrink
-            Console.ReadLine(); //calling ReadLine
-        }
+        return result;
     }
+
+
+    static void Main(string[] args)
+    {
+        string exampleString = null;
+        string result = compress(exampleString);
+        Console.WriteLine(result);
+
+        exampleString = "aabbcc";
+        Console.WriteLine(exampleString);
+        result = compress(exampleString);
+        Console.WriteLine(result);
+
+        exampleString = "aabbccc";
+        Console.WriteLine(exampleString);
+        result = compress(exampleString);
+        Console.WriteLine(result);
+
+        exampleString = "aabbbbbbccccccc";
+        Console.WriteLine(exampleString);
+        result = compress(exampleString);
+        Console.WriteLine(result);
+    }
+}
 }
 
