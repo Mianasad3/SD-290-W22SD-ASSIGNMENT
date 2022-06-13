05c88278426d92c59e081c47d28c8394d7d48b5a
asad <asadtahir9999@gmail.com>
2022-06-13 18:16:47 -0500

    Question one and two completed

--- SD-290-Assignment/SD-290-Assignment/Program.cs
+++ SD-290-Assignment/SD-290-Assignment/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Text;
 
-{
+
 
  class program
 {
@@ -63,5 +63,52 @@ using System.Text;
         Console.WriteLine(result);
     }
 }
-}
+
+
+using System;  //Libarary
+using System.Text;  //Namespace
+
+
+    class Machine
+    {
+
+        private int DAmount;
+        const int cost = 95;
+
+
+        public Machine()
+        {
+            DAmount = 0;
+        }
+        public void DCoin(int coinAmount)
+        {
+            DAmount += coinAmount; //Increment in Damount
+        }
+        public void GetDrink()
+        {
+            if (DAmount >= cost)   //cost is greater then the if work
+            {
+                Console.WriteLine("Your Change is {0} cents", DAmount - cost);  //Printing
+            }
+            else
+                Console.WriteLine("insert more coins");  //if cost is less 
+
+        }
+        public void GetRefund()
+        {
+            Console.WriteLine("You were refunded {0}", DAmount); //Printing
+            DCoin(0);
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("************ Hello from vendor Machine *************** ");//Starting
+            var Machine = new Machine();
+            Machine.DCoin(70);  //Pasing 70 to DCOIN
+            Machine.DCoin(3);   //passing 3 to DCOIN
+            Machine.GetDrink(); //calling Getdrink
+            Console.ReadLine(); //calling ReadLine
+        }
+    }
 
