706719dd12cfada2251abbbfa19c17089af53d3b
asad <asadtahir9999@gmail.com>
2022-06-15 19:05:16 -0500

    Lab2 Submission

--- SD-290-Assignment/SD-290-Assignment/Program.cs
+++ SD-290-Assignment/SD-290-Assignment/Program.cs
@@ -1,116 +1,79 @@
 ﻿using System;
-using System.Text;
 
 {
-
- class program
-{
-    static string compress(string str)
+    static Dictionary<char, int> occurrences = new Dictionary<char, int>();
+    public int GetNumberInput()
     {
-        if (str == null || str == "")//if string is empty return empty
+        int size;
+        Console.WriteLine("Enter your size of int array:");
+        size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++)
         {
-            return str;
+            string intArray = ReadLine();
+            int g = Convert.ToInt32(intArray);
+            arr[i] = g;
         }
-        StringBuilder sb = new StringBuilder();
-        char prevChar = str[0];
-        int count = 1;
-        for (int i = 1; i < str.Length; i++)
+        return arr;
+    }
+    public char getCharacterInput()
+    {
+        int size;
+        Console.WriteLine("Enter your size of char array:");
+        size = Convert.ToInt32(Console.ReadLine());
+        char[] arr = new int[size];
+        for (int i = 0; i < size; i++)
         {
-            if (str[i] == prevChar)
-            {
-                count++;
-            }
-            else
-            {
-                sb.Append(prevChar); //insert char
-                sb.Append(count);//insert count
-                prevChar = str[i];//set previous to current char
-                count = 1;
-            }
+            arr[i] = Console.ReadLine();
         }
-
-        sb.Append(prevChar);
-        sb.Append(count);
-        string result = sb.ToString();//create a string from builder
-
-        if (result.Length >= str.Length)
+        return arr;
+    }
+    public int countchar()
+    {
+        int size; int count = 0;
+        Console.WriteLine("Enter your size of char array:");
+        size = Convert.ToInt32(Console.ReadLine());
+        char[] arr = new int[size];
+        for (int i = 0; i < size; i++)
         {
-            return str;
+            arr[i] = Console.ReadLine();
+            count++;
         }
-        return result;
+        return count;
     }
 
 
+    static void charFuction(char[] characters)
+    {
+        foreach (char c in characters)
+        {
+            if (occurrences.ContainsKey(c))
+            {
+                occurrences[c]++;
+            }
+            else
+            {
+                occurrences.Add(c, 1);
+            }
+        }
+        foreach (char k in occurrences.Keys)
+        {
+            Console.WriteLine(k + " - " + occurrences[k]);
+        }
+    }
+
+    void PrintResults(char countedCharacter, int charFrequency,, int totalChars)
+    {
+        Console.WriteLine(countedCharacter);
+        Console.WriteLine(charFrequency);
+        Console.WriteLine(totalCharas);
+    }
     static void Main(string[] args)
     {
-        string exampleString = null;
-        string result = compress(exampleString);
-        Console.WriteLine(result);
+        char countedCharacter = charFuction();
+        int totalChars = countchar();
+        int charFrequency = getCharacterInput();
+        PrintREsults(countedCharacter, charFrequency, totalChars);
 
-        exampleString = "aabbcc";
-        Console.WriteLine(exampleString);
-        result = compress(exampleString);
-        Console.WriteLine(result);
-
-        exampleString = "aabbccc";
-        Console.WriteLine(exampleString);
-        result = compress(exampleString);
-        Console.WriteLine(result);
-
-        exampleString = "aabbbbbbccccccc";
-        Console.WriteLine(exampleString);
-        result = compress(exampleString);
-        Console.WriteLine(result);
     }
 }
-}
-
-
-using System;  //Libarary
-using System.Text;  //Namespace
-namespace VendorMachine
-{
-    class Machine
-    {
-
-        private int DAmount;
-        const int cost = 95;
-
-
-        public Machine()
-        {
-            DAmount = 0;
-        }
-        public void DCoin(int coinAmount)
-        {
-            DAmount += coinAmount; //Increment in Damount
-        }
-        public void GetDrink()
-        {
-            if (DAmount >= cost)   //cost is greater then the if work
-            {
-                Console.WriteLine("Your Change is {0} cents", DAmount - cost);  //Printing
-            }
-            else
-                Console.WriteLine("insert more coins");  //if cost is less 
-
-        }
-        public void GetRefund()
-        {
-            Console.WriteLine("You were refunded {0}", DAmount); //Printing
-            DCoin(0);
-        }
-
-
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("************ Hello from vendor Machine *************** ");//Starting
-            var Machine = new Machine();
-            Machine.DCoin(70);  //Pasing 70 to DCOIN
-            Machine.DCoin(3);   //passing 3 to DCOIN
-            Machine.GetDrink(); //calling Getdrink
-            Console.ReadLine(); //calling ReadLine
-        }
-    }
-}
-
